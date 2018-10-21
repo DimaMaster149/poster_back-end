45f770a3015355b21749dd9175d3b762475c9bef
DimaMaster149 <dimamaster149@gmail.com>
2018-10-21 22:13:04 +0300

    Add admin controller

--- app/Http/Controllers/Admin/AdminEventController.php
+++ app/Http/Controllers/Admin/AdminEventController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,49 +11,15 @@ use App\State;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image as Image;
 
-
-class EventController extends Controller
+class AdminEventController extends Controller
 {
-    //Список опубликованных ивентов на сегодня
-	public function publishedEvents() {
-		$date = new DateTime();
-		$publishedEvents = Event::orderby('event_id')->where('event_state', 1)->where('event_date', $date->format('Y-m-d'))->get();
-		return response()->json($publishedEvents);
-	}
-	//Список опубликованных ивентов по выбранной дате
-	public function publishedEventsSearch(Request $request) {
-		$events = Event::orderby('event_id')->where('event_state', 1)->where('event_date', $request->event_date)->get();
-		return response()->json($events);
-	}
-	//Список предложенных ивентов определенным пользователем
-	public function proposedEvents() {
-		$userId = Auth::user()->id;
-		$proposedEvents = Event::orderby('event_id')->where('event_state', 2)->where('event_user', $userId)->get();
-		return response()->json($proposedEvents);
-	}
-	//Добавление, редактирование и удаление предолженны событий определенным пользователем
-	public function proposedEventsStore(Request $request) {
-		$file = $request->file('event_image'); //?
-		$extension = $file->getClientOriginalName();
-		$fileName = time().'_'.$extension;
-		$resizedImage = Image::make($file)->resize(640,480);
-		$resizedImage->save('images/'.$fileName);
-		$userId = Auth::user()->id;
-
-		$proposedEvent = new Event($request->all());
-		$proposedEvent->event_image=$fileName;
-		$proposedEvent->event_user = $userId;
-		$proposedEvent->event_state=2;
-		$proposedEvent->save();
-		return response()->json($proposedEvent);
+	//Для опубликованных событий
+	public function publishedAdminEventsEdit($id) {
+		$publishedEvent = Event::find($id);
+		return response()->json($publishedEvent);
 	}
 
-	public function proposedEventsEdit($id) {
-		$proposedEvent = Event::find($id);
-		return response()->json($proposedEvent);
-	}
-
-	public function proposedEventsUpdate(Request $request, $id) {
+	public function publishedAdminEventsUpdate(Request $request, $id) {
 		$file = $request->file('event_image'); //?
 		$extension = $file->getClientOriginalName();
 		$fileName = time().'_'.$extension;
@@ -64,7 +30,7 @@ class EventController extends Controller
 		Event::where('event_id', $id)->update([
 			'event_title' =>  $request->input('event_title'),
 			'event_description' => $request->input('event_description'),
-			'event_state' => 2,
+			'event_state' => 1,
 			'event_user' => $userId,
 			'event_image' =>$fileName,
 			'event_date' => $request->input('event_date'),
@@ -74,7 +40,64 @@ class EventController extends Controller
 		return redirect('/home');
 	}
 
-	public function proposedEventsDelete($id){
+	public function publishedAdminEventsDelete($id){
+		try{
+			Event::destroy($id);
+			return response([], 204);
+		} catch(\Exception $e){
+			return response(['Deleting error', 500]);
+		}
+	}
+
+	//Все предложенные события
+	public function proposedAdminEvents() {
+		$proposedEvents = Event::orderby('event_id')->where('event_state', 2)->get();
+		return response()->json($proposedEvents);
+	}
+
+	public function proposedAdminEventsStore(Request $request) {
+		$file = $request->file('event_image'); //?
+		$extension = $file->getClientOriginalName();
+		$fileName = time().'_'.$extension;
+		$resizedImage = Image::make($file)->resize(640,480);
+		$resizedImage->save('images/'.$fileName);
+		$userId = Auth::user()->id;
+
+		$proposedEvent = new Event($request->all());
+		$proposedEvent->event_image=$fileName;
+		$proposedEvent->event_user = $userId;
+		$proposedEvent->event_state=1;
+		$proposedEvent->save();
+		return response()->json($proposedEvent);
+	}
+
+	public function proposedAdminEventsEdit($id) {
+		$proposedEvent = Event::find($id);
+		return response()->json($proposedEvent);
+	}
+
+	public function proposedAdminEventsUpdate(Request $request, $id) {
+		$file = $request->file('event_image'); //?
+		$extension = $file->getClientOriginalName();
+		$fileName = time().'_'.$extension;
+		$resizedImage = Image::make($file)->resize(640,480);
+		$resizedImage->save('images/'.$fileName);
+		$userId = Auth::user()->id;
+
+		Event::where('event_id', $id)->update([
+			'event_title' =>  $request->input('event_title'),
+			'event_description' => $request->input('event_description'),
+			'event_state' => 1,
+			'event_user' => $userId,
+			'event_image' =>$fileName,
+			'event_date' => $request->input('event_date'),
+			'event_time' => $request->input('event_time'),
+			'event_location' => $request->input('event_location'),
+		]);
+		return redirect('/home');
+	}
+
+	public function proposedAdminEventsDelete($id){
 		try{
 			Event::destroy($id);
 			return response([], 204);
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,20 @@ Route::get('/home', 'HomeController@index')->name('home');
 //published events for users
 Route::get('/events/published', 'User\EventController@publishedEvents');
 Route::post('/events/published/search', 'User\EventController@publishedEventsSearch');
-//proposed event for users
+//proposed events for one user
 Route::get('/events/proposed', 'User\EventController@proposedEvents');
 Route::post('/events/proposed/store', 'User\EventController@proposedEventsStore');
 Route::get('/events/proposed/{id?}', 'User\EventController@proposedEventsEdit');
 Route::post('/events/proposed/{id?}', 'User\EventController@proposedEventsUpdate');
 Route::delete('/events/proposed/delete/{id?}', 'User\EventController@proposedEventsDelete');
+
+//proposed events for admin
+Route::get('/admin/events/proposed', 'Admin\AdminEventController@proposedAdminEvents');
+Route::post('/admin/events/proposed/store', 'Admin\AdminEventController@proposedAdminEventsStore');
+Route::get('/admin/events/proposed/{id?}', 'Admin\AdminEventController@proposedAdminEventsEdit');
+Route::post('/admin/events/proposed/{id?}', 'Admin\AdminEventController@proposedAdminEventsUpdate');
+Route::delete('/admin/events/proposed/delete/{id?}', 'Admin\AdminEventController@proposedAdminEventsDelete');
+//published events for admin
+Route::get('/admin/events/published/{id?}', 'Admin\AdminEventController@publishedAdminEventsEdit');
+Route::post('/admin/events/published/{id?}', 'Admin\AdminEventController@publishedAdminEventsUpdate');
+Route::delete('/admin/events/published/delete/{id?}', 'Admin\AdminEventController@publishedAdminEventsDelete');
